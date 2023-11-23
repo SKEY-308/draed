2b40c52dfdcf4a42630b63ea815b82f051fac447
Your Name <severus.key308@gmail.com>
2023-11-23 22:00:12 +0400

    22'00

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -61,9 +61,9 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         if (hasImageChanged) {
             const imgRes = await startUpload(files);
 
-            // if (imgRes && imgRes[0].fileUrl) {
-            //     values.profile_photo = imgRes[0].fileUrl;
-            // }
+            if (imgRes && imgRes[0].fileUrl) {
+                values.profile_photo = imgRes[0].fileUrl;
+            }
         }
 
         await updateUser({
