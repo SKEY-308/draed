3a8d5aa877ca7b82da7ad361c0f6fdaf803aa028
Your Name <severus.key308@gmail.com>
2023-11-27 20:17:44 +0400

    20'17

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -10,15 +10,15 @@ import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
 export async function fetchUser(userId: string) {
-  // try {
-  //   connectToDB();
-  //   return await User.findOne({ id: userId }).populate({
-  //     path: "communities",
-  //     model: Community,
-  //   });
-  // } catch (error: any) {
-  //   throw new Error(`Failed to fetch user: ${error.message}`);
-  // }
+  try {
+    connectToDB();
+    return await User.findOne({ id: userId }).populate({
+      path: "communities",
+      model: Community,
+    });
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user: ${error.message}`);
+  }
 }
 
 interface Params {
